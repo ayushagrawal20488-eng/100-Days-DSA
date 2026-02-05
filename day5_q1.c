db5969e3acfefcfd77ef6111d21b5099418a4d72
ayushagrawal20488-eng <ayushagrawal20488@gmail.com>
2026-02-05 23:15:21 +0530

    Add files via upload

---
+++ day5_q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    int p;
+    scanf("%d", &p);
+    int log1[p];
+    for(int i = 0; i < p; i++) {
+        scanf("%d", &log1[i]);
+    }
+    int q;
+    scanf("%d", &q);
+    int log2[q];
+    for(int i = 0; i < q; i++) {
+        scanf("%d", &log2[i]);
+    }
+    int merged[p + q];
+    int i = 0, j = 0, k = 0;
+    while(i < p && j < q) {
+        if(log1[i] <= log2[j]) {
+            merged[k++] = log1[i++];
+        } else {
+            merged[k++] = log2[j++];
+        }
+    }
+    while(i < p) {
+        merged[k++] = log1[i++];
+    }
+    while(j < q) {
+        merged[k++] = log2[j++];
+    }
+    for(int idx = 0; idx < k; idx++) {
+        printf("%d ", merged[idx]);
+    }
+    printf("\n");   
+    return 0;
+}
